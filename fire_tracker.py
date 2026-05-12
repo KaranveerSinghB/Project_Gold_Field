cebeb60851f4206212bc913e913e53bfdb8d0b5b
KaranveerSinghB <kvsbrar2006@gmail.com>
2026-05-12 17:32:02 -0400

    Update fire_tracker.py

--- fire_tracker.py
+++ fire_tracker.py
@@ -41,8 +41,7 @@ st.set_page_config(page_title="Punjab Fire Tracker", layout="wide", initial_side
 
 hide_st_style = """
     <style>
-    [data-testid="stToolbar"] {display: none !important;}
-    footer {display: none !important;}
+    footer {visibility: hidden !important;}
     </style>
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
